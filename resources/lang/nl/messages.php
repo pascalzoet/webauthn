23ae02df93ce99289480092ada69acd8ba26d522
pascalzoet <pascalzoet@users.noreply.github.com>
2022-02-07 11:25:50 +0100

    Create messages.php

---
+++ resources/lang/nl/messages.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+    'buttonAdvise' => 'Als uw beveiligingssleutel een knop heeft, drukt u erop.',
+    'noButtonAdvise' => 'Als dit niet het geval is, verwijdert u het en plaatst u het opnieuw.',
+    'success' => 'Uw sleutel wordt gedetecteerd en gevalideerd.',
+    'insertKey' => 'Voer uw beveiligingssleutel in',
+    'cancel' => 'Cancel',
+
+    'auth' => [
+        'title' => 'Authenticatie met een beveiligingssleutel',
+    ],
+
+    'register' => [
+        'title' => 'Registreer een nieuwe beveiligingssleutel',
+    ],
+];
