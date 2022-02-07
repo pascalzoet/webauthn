a4a8da48f736a2bc46841e1a55e6ca065491439d
pascalzoet <pascalzoet@users.noreply.github.com>
2022-02-07 11:25:31 +0100

    Create errors.php

---
+++ resources/lang/nl/errors.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'user_unauthenticated' => 'U moet inloggen voordat u een Webauthn-verificatie uitvoert',
+    'auth_data_not_found' => 'Authenticatiegegevens niet gevonden',
+    'create_data_not_found' => 'Registratie data niet gevonden',
+    'cannot_register_new_key' => 'U moet inloggen voordat u een nieuwe Webauthn-verificatie toevoegt',
+    'object_not_found' => 'Object niet gevonden',
+    'not_supported' => 'Uw browser ondersteunt momenteel geen WebAuthn.',
+    'not_secured' => 'WebAuthn ondersteunt alleen beveiligde verbindingen. Voor testen via HTTP kunt u de oorsprong "localhost" gebruiken.',
+    'key_already_used' => 'Deze sleutel is al geregistreerd. Het is niet nodig om het opnieuw te registreren.',
+    'key_not_allowed' => 'De bewerking is verlopen of niet toegestaan.',
+
+];
